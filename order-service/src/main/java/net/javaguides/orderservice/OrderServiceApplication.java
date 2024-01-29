bbd9dd004e866581d7a87b9715d1686d07bfab86
Saloni Chauhan <schauhan1@hawk.iit.edu>
2024-01-28 17:49:47 -0700

    added comments how to start up

--- order-service/src/main/java/net/javaguides/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/net/javaguides/orderservice/OrderServiceApplication.java
@@ -11,5 +11,8 @@ public class OrderServiceApplication {
 	}
 
 	// remember to start the zookeeper and the server first in order to be able to see the orders in stock and email service logs
+	// go to kafka folder using cd whereever you have saved it
+	// first start the zookeeper bash using this command - bin/zookeeper-server-start.sh config/zookeeper.properties
+	// second start the server bash using this command - bin/kafka-server-start.sh config/server.properties
 
 }
